1ec18bc73ea0a4d1537138c9ad38c6b01eab520e
insa14 <kong3361@naver.com>
2022-02-28 21:06:16 +0900

    📝Correction auth file
    문서 위치 변경 및 router설정

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const passportConfig = require('./passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 app.use(passport.initialize());
 passportConfig();
 // catch 404 and forward to error handler
